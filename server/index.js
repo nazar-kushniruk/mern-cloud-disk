46db6a462ec65312ae16fe0fc31325f69eb27896
nazar-kushniruk <kushniruknazar@gmail.com>
2020-12-01 02:23:16 +0200

    add registration and login to beckand

--- server/index.js
+++ server/index.js
@@ -1,15 +1,25 @@
 const express = require("express");
 const mongoose = require('mongoose');
 const config = require('config');
+const authRouter = require('./routes/auth.routes')
 
 const app = express();
 const PORT = config.get('serverPort');
+
+app.use(express.json());
+app.use('/auth', authRouter);
+
+
 const start = async () => {
     try {
-        mongoose.connect(config.get('dbUrl'));
+        await mongoose.connect(config.get('dbUrl'),  { useUnifiedTopology: true, useNewUrlParser: true  });
         app.listen(PORT, console.log('Server started on port', PORT));
+        
+        // app.get('auth', function (req, res) {
+        //     res.send('<h1>hello world</h1>')
+        //   })
     } catch (e) {
-
+        console.log('start ERRORS ->', e);
     }
 }
 
